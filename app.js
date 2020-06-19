11149eb1c1a3bbcefb7d15108a4c311f98eb5030
Emad <elbembo@users.noreply.github.com>
2020-06-19 02:02:19 +0200

    sd

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
+  const VERIFY_TOKEN = "Wc1m1vIZBAxyQEsbFXYVn43twEreOF62RZB0ZCMdTJ77GU";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
